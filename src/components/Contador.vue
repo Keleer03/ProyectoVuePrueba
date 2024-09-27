cfdb220c28bcc05327dc630f9a024906d8fbcb74
DESKTOP-G8H3VQF\Usuario <kellergb02@gmail.com>
2024-09-27 14:03:31 -0600

    Commit 2 - Manejo de componente Contador

---
+++ src/components/Contador.vue
@@ -0,0 +1,27 @@
+// eslint-disable-next-line vue/multi-word-component-names
+<template>
+  <h1>Hola mundo</h1>
+  <p>{{ contador }}</p>
+
+  <button @click="incrementar">Incrementar valor</button>
+  <button @click="decrementar">Decrementar valor</button>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const contador = ref(0)
+
+const incrementar = () => {
+  contador.value++
+}
+
+const decrementar = () => {
+  contador.value--
+}
+</script>
+
+<style scoped>
+p {
+  color: green;
+}
+</style>
